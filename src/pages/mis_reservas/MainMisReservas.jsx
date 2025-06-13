eb95a8aec1a83263421c9d5ae76fc92e8719be37
Nicolasinigo <ninigo93@gmail.com>
2025-06-13 10:49:51 -0300

    agregamos el modal para visualizar las reservas realizadas

--- src/pages/mis_reservas/MainMisReservas.jsx
+++ src/pages/mis_reservas/MainMisReservas.jsx
@@ -1,7 +1,21 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-const MainMisReservas = () => {
+const MainMisReservas = ({show, onHide}) => {
   return (
-    <div>MainMisReservas</div>
+    <Modal show={show} onHide={onHide} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Mis Reservas</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>reservas realizadas</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Cerrar
+        </Button>
+      </Modal.Footer>
+    </Modal>
   )
 }
 
--- src/pages/reservar/MainReservar.jsx
+++ src/pages/reservar/MainReservar.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Cards from "./Cards";
 import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Button';
+import MainMisReservas from "../mis_reservas/MainMisReservas";
+
 
 const MainReservar = () => {
+
+  const [mostrarModal, setMostrarModal] = useState(false);
+
   return (
     <div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">LOGO</Navbar.Brand>
           <Nav>
-            <Nav.Link href="/mis-reservas">
+            <Nav.Link onClick={() => setMostrarModal(true)}>
               Mis Reservas <Badge bg="secondary">1</Badge>
             </Nav.Link>
             <Nav.Link href="/info-usuario">Usuario</Nav.Link>
@@ -28,6 +33,7 @@ const MainReservar = () => {
         <Cards></Cards>
         <Cards></Cards>
       </Container>
+      <MainMisReservas show={mostrarModal} onHide={() => setMostrarModal(false)} />
     </div>
   );
 };
