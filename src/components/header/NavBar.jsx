6f7ecc64ea826828180d874acab8eb4e9e3d26a9
Camila Juarez <camilajuarez17@gmail.com>
2025-06-28 16:06:53 -0300

    modifique el navbar para los rolres y usuarios

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useStore } from "../../store/AuthStore";
 import "../../css/modo_claro/ModoClaro.css";
@@ -8,6 +8,55 @@ import { FaSun, FaMoon } from "react-icons/fa";
 
 const NavBar = () => {
   const { color, changeColor } = useStore();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("usuario");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("usuario"); // Elimina el usuario del localStorage
+    setUser(null); // Limpia el estado local
+    <Link to="/"></Link>
+  };
+
+  let sinLoguear = "";
+  let linksClientes = "";
+  let linksRoles = "";
+  let Desloguearboton = "";
+
+  if (!user) {
+    sinLoguear = (
+      <>
+        <Link to="/login">Iniciar Sesión</Link>
+        <Link to="/register">Registrarse</Link>
+      </>
+    );
+  } else {
+    linksClientes = (
+      <>
+        <Link to="/reservas">Reservas</Link>
+        <Link to="/mis-reservas">Mis Reservas</Link>
+      </>
+    );
+
+    if (user.rol === "empleado") {
+      linksRoles = <Link to="/caja">Caja</Link>;
+    }
+
+    if (user.rol === "admin") {
+      linksRoles = <Link to="/admin">Admin</Link>;
+    }
+
+    Desloguearboton = (
+      <button onClick={handleLogout} className="logout-button">
+        Cerrar Sesión
+      </button>
+    );
+  }
 
   return (
     <div
@@ -16,15 +65,18 @@ const NavBar = () => {
       }`}
     >
       <nav className="navbar-custom">
-  <div className="logo-title">
-    <Link to="/">
-      <img src="/icono.png" alt="Logo" className="logo-img" />
-    </Link>
-    <h2 className="title-principal">MIS CANCHAS</h2>
-  </div>
+        <div className="logo-title">
+          <Link to="/">
+            <img src="/icono.png" alt="Logo" className="logo-img" />
+          </Link>
+          <h2 className="title-principal">MIS CANCHAS</h2>
+        </div>
         <div className="nav-links">
-          <Link to="/login">Iniciar Sesión</Link>
-          <Link to="/register">Registrarse</Link>
+          {sinLoguear}
+          {linksClientes}
+          {linksRoles}
+          {Desloguearboton}
+
           <button
             className="color-toggle"
             onClick={() => changeColor(color === "Claro" ? "Oscuro" : "Claro")}
